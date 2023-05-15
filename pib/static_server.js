00b38cb4a46637ecdea69579ee23bc5acb6c5ce3
gittest <gugo2828@gmail.com>
2023-05-15 22:52:34 +0200

    Project 69,all project with single commit from old repo

---
+++ pib/static_server.js
@@ -0,0 +1,51 @@
+/*
+1.
+node-static (Node.js)
+$ npm install -g node-static   # install dependency
+$ static -p 8000
+node static_server.js command
+2.
+https://github.com/http-party/http-server
+http-server D:\Folder
+npm install http-server -g
+*/ 
+
+
+
+var http = require("http"),
+url = require("url"),
+path = require("path"),
+fs = require("fs")
+port = process.argv[2] || 8888;
+
+http.createServer(function(request, response) {
+
+var uri = url.parse(request.url).pathname
+, filename = path.join(process.cwd(), uri);
+
+path.exists(filename, function(exists) {
+if(!exists) {
+  response.writeHead(404, {"Content-Type": "text/plain"});
+  response.write("404 Not Found\n");
+  response.end();
+  return;
+}
+
+if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+fs.readFile(filename, "binary", function(err, file) {
+  if(err) {        
+    response.writeHead(500, {"Content-Type": "text/plain"});
+    response.write(err + "\n");
+    response.end();
+    return;
+  }
+
+  response.writeHead(200);
+  response.write(file, "binary");
+  response.end();
+});
+});
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
